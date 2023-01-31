ce7a913d08019d1cbfb6737a3ffeb3a96baf4a24
Victoria <victoria.golomazova@yandex.ru>
2023-01-31 16:50:41 +0300

    calendar done

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
+import { DateService } from '../shared/date.service';
+import { Week } from '../shared/types';
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.scss']
 })
-export class CalendarComponent {
+export class CalendarComponent implements OnInit {
+  public calendar: Week[] = [];
 
+  constructor(
+    private _dateService: DateService,
+  ) { }
+
+  ngOnInit(): void {
+    this._dateService.date.subscribe(this.generate.bind(this));
+  }
+
+  private generate(now: moment.Moment) {
+    const startDay = now.clone().startOf('month').startOf('week');
+    const endDay = now.clone().endOf('month').endOf('week');
+
+    const date = startDay.clone().subtract(1, 'day')
+
+    const calendar = []
+
+    while (date.isBefore(endDay, 'day')) {
+      calendar.push({
+        days: Array(7)
+          .fill(0)
+          .map(() => {
+            const value = date.add(1, 'day').clone();
+            const isActive = moment().isSame(value, 'date');
+            const isDisabled = !now.isSame(value, 'month');
+            const isSelected = now.isSame(value, 'date');
+
+            return {
+              value, isActive, isDisabled, isSelected
+            }
+          })
+      })
+    }
+
+    this.calendar = calendar;
+  }
+
+  public select(day: moment.Moment) {
+    this._dateService.changeDate(day)
+  }
 }
